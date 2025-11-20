d115302d8a12ffb4cb591e53452ceb214d76b40f
BenyiCordero <benyi.uriel2006@gmail.com>
2025-11-19 19:47:42 -0600

    Se agregan las excepciones personalizadas y el manejador de excepciones

---
+++ src/main/java/com/bcss/sistemaventas/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.bcss.sistemaventas.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EmptyObjectException.class)
+    public ResponseEntity<String> handleEmptyObjectException(EmptyObjectException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+    @ExceptionHandler(InvalidEntryException.class)
+    public ResponseEntity<String> handleInvalidEntryException(InvalidEntryException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<String> handleNotFoundException(NotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+    @ExceptionHandler(RepeatedException.class)
+    public ResponseEntity<String> handleRepeatedException(RepeatedException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+}
